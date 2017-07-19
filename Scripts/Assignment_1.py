c587283700e3d67ebb627f990000f0f472509aa5
Shawn <da_capo_usa@hotmail.com>
2017-07-19 08:23:22 +0800

    CENG 420 Assignment 3 Q1 Final version

--- Scripts/Assignment_1.py
+++ Scripts/Assignment_1.py
@@ -1,7 +1,7 @@
 from KNN_Algorithm import knn
 import sys
 
-if __name__=="__main__":
+def main():
     A = knn('iris.csv')
     neighbors = []
     setosa = A.setosaSet()
@@ -14,16 +14,16 @@ if __name__=="__main__":
 
     for item in test_set:
         neighbors = A.getNeighbors(train_set, item)
-        print 'The closet neighbor of %s is:'%str(item)
+        print 'The closet neighbor of %s is:' % str(item)
         print neighbors[0][0], 'with a distance of ', neighbors[0][1]
-        print 'The type of test data %s is:' %str(item), neighbors[0][0][-1], '\n'
+        print 'The type of test data %s is:' % str(item), neighbors[0][0][-1], '\n'
 
     print 'Max distance:', A.max_dist
     print '************************************************************************', '\n'
     print 'Step 2:'
     print 'Random picked 5 setosa test set for step 2(not included in the set in step 1):'
     test2set = A.getTestSet()
-    print test2set,'\n'
+    print test2set, '\n'
 
     for item in test2set:
         neighbors = A.getNeighbors(train_set, item, 'P2')
@@ -31,19 +31,23 @@ if __name__=="__main__":
             iris_type = 'Versicolor'
         else:
             iris_type = 'Setosa'
-        print 'The closet neighbor of %s is:'%str(item)
+        print 'The closet neighbor of %s is:' % str(item)
         print neighbors[0][0], 'with a distance of ', neighbors[0][1]
-        print 'The type of test data %s is:' %str(item), iris_type, '\n'
+        print 'The type of test data %s is:' % str(item), iris_type, '\n'
     versicolor = A.versicolorSet()
     ver_test_set = A.getVersicolorTestSet()
     print '\nRandom picked 5 versicolor test set'
-    print ver_test_set,'\n'
+    print ver_test_set, '\n'
     for item in ver_test_set:
         neighbors = A.getNeighbors(train_set, item, 'P2')
         if float(neighbors[0][1]) > float(A.max_dist):
             iris_type = 'Versicolor'
         else:
             iris_type = 'Setosa'
-        print 'The closet neighbor of %s is:'%str(item)
+        print 'The closet neighbor of %s is:' % str(item)
         print neighbors[0][0], 'with a distance of ', neighbors[0][1]
-        print 'The type of test data %s is:' %str(item), iris_type, '\n'
+        print 'The type of test data %s is:' % str(item), iris_type, '\n'
+
+
+if __name__=="__main__":
+    main()
